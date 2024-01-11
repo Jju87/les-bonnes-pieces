060bde26ffb370056d22ccda0a7d4cdcfe38d463
Julien <gardier.julien@gmail.com>
2024-01-11 19:25:32 +0100

    added functional filters for buttons 'croissant' and 'prix décroissant' by using innerHtml in order to erase content of .fiches and then make our filtered list appear

--- pieces.js
+++ pieces.js
@@ -1,29 +1,114 @@
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
-const pieces = await reponse.json();
-// Création des balises
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(Aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Aucune description pour le moment)";
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "En rupture de stock";
-//j'avais écrit ça **************article.disponibilite = true ? "En stock" : "En rupture de stock"; Pas la peine de mettre le = true!
+// Wrap your code in an async function
+async function fetchDataAndGeneratePieces() {
+    // Récupération des pièces depuis le fichier JSON
+    const reponse = await fetch("pieces-autos.json");
+    const pieces = await reponse.json();
 
-const ficheProduit = document.querySelector(".fiches");
-ficheProduit.append(imageElement, nomElement, prixElement, categorieElement, descriptionElement, disponibiliteElement);
+    // Efface le contenu de la balise body et donc l’écran
+    document.querySelector(".fiches").innerHTML = '';
+
+    function genererPieces(pieces) {
+        for (let i = 0; i < pieces.length; i++) {
+            const article = pieces[i];
+
+            // Récupération de l'élément du DOM qui accueillera les fiches
+            const sectionFiches = document.querySelector(".fiches");
+            // Création d’une balise dédiée à une pièce automobile
+            const pieceElement = document.createElement("article");
+            // Création des balises 
+            const imageElement = document.createElement("img");
+            imageElement.src = article.image;
+            const nomElement = document.createElement("h2");
+            nomElement.innerText = article.nom;
+            const prixElement = document.createElement("p");
+            prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+            const categorieElement = document.createElement("p");
+            categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+            const descriptionElement = document.createElement("p");
+            descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+            const stockElement = document.createElement("p");
+            stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+            // On rattache la balise article a la section Fiches
+            sectionFiches.appendChild(pieceElement);
+            // On rattache l’image à pieceElement (la balise article)
+            pieceElement.appendChild(imageElement);
+            pieceElement.appendChild(nomElement);
+            pieceElement.appendChild(prixElement);
+            pieceElement.appendChild(categorieElement);
+            //Ajout des éléments au DOM pour l'exercice
+            pieceElement.appendChild(descriptionElement);
+            pieceElement.appendChild(stockElement);
+        }
+    }
+
+    genererPieces(pieces);
+
+    const boutonTrier = document.querySelector(".btn-trier")
+    boutonTrier.addEventListener("click", function () {
+        const piecesOrdonnees = Array.from(pieces);
+        piecesOrdonnees.sort(function (a, b) {
+            return b.prix - a.prix;
+        });
+        document.querySelector(".fiches").innerHTML = ""
+        genererPieces(piecesOrdonnees)
+
+    });
+
+    const btnCroissant = document.querySelector(".btn-croissant");
+    btnCroissant.addEventListener("click", () => {
+        const piecesOrdonnees = Array.from(pieces)
+        piecesOrdonnees.sort(function (a, b) {
+            return a.prix - b.prix
+        })
+        document.querySelector(".fiches").innerHTML = "";
+        genererPieces(piecesOrdonnees)
+
+    });
+
+    
+}
+
+// Call the async function
+fetchDataAndGeneratePieces();
 
 
 
+ const names = pieces.map(piece =>piece.nom)
+ const abordablesElement = document.querySelector(".abordables")
+ const listNames = document.createElement("ul")
+
+ const describe = pieces.map(piece => piece.description)
+ const price = pieces.map(piece => piece.prix)
+
+ 
+
+ 
+ //Création de la liste
+//Ajout de chaque nom à la liste
+for(let i=0; i < names.length ; i++){
+   
+
+    if (pieces[i].disponibilite === true) {
+        const nomElement = document.createElement('li');
+        nomElement.innerText = names[i];
+
+        const describeElement = document.createElement("p")
+        describeElement.innerText =describe[i]
+
+        const priceElement = document.createElement("p")
+        priceElement.innerText = ` ${price[i]} € `
 
 
+        nomElement.appendChild(describeElement)
+        listNames.appendChild(nomElement)
+        nomElement.appendChild(priceElement)
+    }
+ }
+ 
+ abordablesElement.appendChild(listNames)
+
+ 
 
 
 
